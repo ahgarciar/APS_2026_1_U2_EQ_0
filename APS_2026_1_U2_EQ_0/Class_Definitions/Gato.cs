8708c50d838cd16fafbb75583f0d677b2584e899
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2026-01-21 22:37:50 -0600

    Initial commit

---
+++ APS_2026_1_U2_EQ_0/Class_Definitions/Gato.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APS_2026_1_U2_EQ_0.Class_Definitions
+{
+    internal class Gato
+    {
+        //Abstracción..,
+
+        //modificadores de acceso ... : private / public
+        private int id_gato;
+        string nombre;
+        public string color;
+        private int edad;
+        private bool estaVacunado;
+        private double peso;
+
+        public string Nombre() {
+            return nombre;         
+        }
+
+        public void Nombre(string nombre) {
+            this.nombre = nombre;
+        }
+
+    }
+}
---
+++ APS_2026_1_U2_EQ_0/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using APS_2026_1_U2_EQ_0.Class_Definitions;
+
+namespace APS_2026_1_U2_EQ_0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //Tipo de dato abstracto
+            Gato gato; //declaracion del objeto
+
+            gato = new Gato();
+
+            gato.color = "Naranja";
+
+            gato.Nombre("Gatito");
+
+            string n = gato.Nombre();
+
+            txt_nombre.Text = n;
+
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
