b142eb78d1870642d0f92359f57d8762618f65f7
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2026-02-19 14:07:17 -0600

    ll

--- Clase_ICDIA_Unidad1/Clase_ICDIA_Unidad1/obj/Debug/net8.0/Clase_ICDIA_Unidad1.AssemblyInfo.cs
+++ Clase_ICDIA_Unidad1/Clase_ICDIA_Unidad1/obj/Debug/net8.0/Clase_ICDIA_Unidad1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Clase_ICDIA_Unidad1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d20530d8b1d31e9b2c3eed5dab990107482fd76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ae469772193b16fc9c7ae8a035fddd0c58181d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Clase_ICDIA_Unidad1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Clase_ICDIA_Unidad1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
